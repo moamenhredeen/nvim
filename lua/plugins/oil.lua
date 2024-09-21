43d1729b605c96d8007c90fcca18565570763353
Moamen Hraden <moamen.hraden@primion.eu>
2024-09-21 12:37:01 +0200

    update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,24 @@
 return {
 	'stevearc/oil.nvim',
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	config = function ()
+		local oil = require('oil')
+		oil.setup {
+			default_file_explorer = false,
+			skip_confirm_for_simple_edits = true,
+		  	-- (:help prompt_save_on_select_new_entry)
+  			prompt_save_on_select_new_entry = false,
+
+			-- Configuration for the floating window in oil.open_float
+			float = {
+				-- Padding around the floating window
+				padding = 2,
+				max_width = 100,
+				max_height = 30,
+				-- border = "rounded",
+			},
+		}
+
+ 		vim.keymap.set('n', '<leader>E', oil.open_float, { silent = true } )
+	end
 }
