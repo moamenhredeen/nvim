85081f67f655223ce2bcd71da4a7f9809542326d
Moamen Hraden <moamen.hraden@primion.eu>
2025-03-07 16:21:51 +0100

    diable warning for the treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
-		require('nvim-treesitter.configs').setup({
+		---@diagnostic disable
+		require('nvim-treesitter.configs').setup {
 			ensure_installed = {
 				"lua",
 				"vim",
@@ -47,7 +48,7 @@ return {
 					},
 				},
 			},
-		})
+		}
 
 		vim.wo.foldmethod = "expr"
 		vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
