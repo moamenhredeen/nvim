e6d15dd571fff7e1090266168f60827d3017b445
Moamen Hraden <moamen.hraden@primion.eu>
2025-03-10 00:03:09 +0100

    update cmp-blink keymap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,8 +3,20 @@ return {
 	version = '*',
 	opts = {
 		keymap = {
-			preset = 'super-tab',
-			['<CR>'] = { 'accept', 'fallback' },
+			preset = "enter",
+			['<C-e>'] = { 
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.hide()
+					else
+						return cmp.show()
+					end
+				end,
+				"fallback",
+			},
+      ['<C-space>'] = {},
+			['<Tab>'] = {},
+			['<S-Tab>'] = {},
 		},
 		appearance = {
 			use_nvim_cmp_as_default = true,
